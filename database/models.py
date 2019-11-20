32e7431bd5f3a4df77e57d8c6a4ec3acf1bd3c58
r0somak <mikolajakartel@gmail.com>
2019-11-20 14:29:29 +0100

    Dodanie kolejnych modeli

--- database/models.py
+++ database/models.py
@@ -6,10 +6,31 @@ from django.contrib.auth.models import AbstractUser
 class Lekarz(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    specjalizacja = models.CharField(max_length=100)
+    specjalizacja = models.CharField(max_length=100, null=True)
     nr_pwz = models.CharField(max_length=100)
-    email = models.EmailField()
-    telefon = models.CharField(max_length=100)
+    email = models.EmailField(null=True)
+    telefon = models.CharField(max_length=100, null=True)
+
+
+class Przychodnia(models.Model):
+    nazwa = models.CharField(max_length=100)
+    uica = models.CharField(max_length=100)
+    nr_ulicy = models.CharField(max_length=100)
+    nr_mieszkania = models.CharField(max_length=100, null=True)
+    kod_pocztowy = models.CharField(max_length=100)
+    miasto = models.CharField(max_length=100)
+    wojewodztwo = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, null=True)
+    telefon = models.CharField(max_length=100, null=True)
+    lekarze = models.ManyToManyField(Lekarz)
+
+
+def user_dir_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+class Dokument(models.Model):
+    dokument = models.FileField(upload_to=user_dir_path)
 
 
 class Pacjent(models.Model):
@@ -18,12 +39,13 @@ class Pacjent(models.Model):
     pesel = models.CharField(max_length=100)
     ulica = models.CharField(max_length=100)
     nr_ulicy = models.CharField(max_length=20)
-    nr_mieszkania = models.CharField(max_length=20)
+    nr_mieszkania = models.CharField(max_length=20, null=True)
     kod_pocztowy = models.CharField(max_length=10)
     miasto = models.CharField(max_length=100)
     wojewodztwo = models.CharField(max_length=100)
-    email = models.EmailField()
-    telefon = models.CharField(max_length=100)
+    email = models.EmailField(null=True)
+    telefon = models.CharField(max_length=100, null=True)
+    dokumenty = models.ManyToManyField(Dokument, null=True)
 
 
 class CustomUser(AbstractUser):
@@ -40,3 +62,25 @@ class CustomUser(AbstractUser):
         blank=True,
         null=True,
     )
+
+
+class Wizyta(models.Model):
+    data_wizyty = models.DateTimeField()
+    uwagi = models.CharField(max_length=500)
+
+    fk_id_pacjent = models.ForeignKey(
+        Pacjent,
+        on_delete=models.CASCADE,
+    )
+
+    fk_id_lekarz = models.ForeignKey(
+        Lekarz,
+        on_delete=models.CASCADE,
+    )
+
+    fk_id_przychodnia = models.ForeignKey(
+        Przychodnia,
+        on_delete=models.CASCADE,
+    )
+
+    dokumenty = models.ManyToManyField(Dokument, null=True)
