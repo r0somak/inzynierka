f5fbfe3a47c6870fd611f9f2004898afc72b96f2
magiczna7 <olabartos7@gmail.com>
2019-11-20 17:22:44 +0100

    Dodanie nowych ficzerów

---
+++ frontend/src/components/BackgroundImageHalf.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="backgroundImage">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackgroundImageHalf',
+};
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap');
+
+  .backgroundImage {
+   background-image: url('../assets/medbackground.jpg');
+    width: 50%;
+    min-height: 800px;
+    background-position: top left;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
