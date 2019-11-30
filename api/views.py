91d23f80eb6c6a089e0e8071aa8a5c2a7d96be06
r0somak <mikolajakartel@gmail.com>
2019-11-30 23:45:22 +0100

    wystawienie api do uzyskania authtoken

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import status
 
 from django.contrib.auth import authenticate
+
 from .serializers import UserSerializer
 
 from database.models import CustomUser
@@ -18,6 +22,7 @@ class ApiRoot(generics.GenericAPIView):
         return Response(
             {
                 'create_user': reverse(UserCreate.name, request=request),
+                'user_login': reverse(GetAuthToken.name, request=request),
             }
         )
 
@@ -27,3 +32,21 @@ class UserCreate(generics.CreateAPIView):
     authentication_classes = ()
     permission_classes = ()
     serializer_class = UserSerializer
+
+
+class GetAuthToken(APIView):
+    name = 'user-login'
+    permission_classes = ()
+
+    def post(self, request, ):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({
+                'token': user.auth_token.key
+            })
+        else:
+            return Response({
+                'error': "Wrong Credentials"
+            }, status=status.HTTP_400_BAD_REQUEST)
