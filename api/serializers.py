5691841b4c5d347d07d41edbf7462a07a8a3caf4
r0somak <mikolajakartel@gmail.com>
2019-11-30 21:24:06 +0100

    Serializacja: Utworzenie powiązania pomiędzy CustomUser -> Pacjent

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework.authtoken.models import Token
 from database.models import CustomUser, Pacjent
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = (
@@ -20,13 +20,14 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     def create(self, validated_data):
         pacjent = Pacjent()
+        pacjent.save()
         user = CustomUser(
             email=validated_data['email'],
             username=validated_data['username'],
-            fk_id_pacjent=pacjent.id,
+            fk_id_pacjent=pacjent,
         )
         user.set_password(validated_data['password'])
-        pacjent.save()
+
         user.save()
         Token.objects.create(user=user)
         return user
