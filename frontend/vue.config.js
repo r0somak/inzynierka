678a69ae8136d073f4bde94ff37973a0290eac0d
r0somak <mikolajakartel@gmail.com>
2019-12-04 01:23:46 +0100

    Włączenie https

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
       .port(8080)
       .hotOnly(true)
       .watchOptions({ poll: 1000 })
-      .https(false)
+      .https(true)
       // eslint-disable-next-line no-useless-escape
       .headers({ 'Access-Control-Allow-Origin': '*' });
   },
